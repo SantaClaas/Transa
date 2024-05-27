e943c78e9b4fcdd19b2bf76e8028c5739580b757
Claas <claas@claas.dev>
2024-05-28 00:57:49 +0200

    Try other deploy to fix 404 error

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  ssr: true,
   server: {
-    preset: "github-pages",
+    baseURL: process.env.BASE_PATH,
+    preset: "static",
   },
 });
